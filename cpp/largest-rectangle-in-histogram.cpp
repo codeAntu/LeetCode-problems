a67a31175a76097e2bc41fd2d89124ff84bfb9b5
Ananta Karmakar <codeantu@gmail.com>
2024-05-16 10:47:58 +0530

    -

---
+++ cpp/largest-rectangle-in-histogram.cpp
@@ -0,0 +1,44 @@
+#include <stack>
+#include <utility>
+
+#include "iostream"
+#include "map"
+#include "math.h"
+#include "vector"
+
+using namespace std;
+
+class Solution {
+public:
+  int largestRectangleArea(vector<int>& heights) {
+    int max = 0;
+    stack<pair<int, int>> s;
+    int start = 0;
+    for (int i = 0; i < heights.size(); i++) {
+      start = i;
+      while (!s.empty() && s.top().second > heights[i]) {
+        pair<int, int> curr = s.top();
+        s.pop();
+        max = std::max(max, curr.second * (i - curr.first));
+        start = curr.first;
+      }
+      s.push({start, heights[i]});
+    }
+
+    while (!s.empty()) {
+      pair<int, int> curr = s.top();
+      s.pop();
+      max = max > curr.second * (heights.size() - curr.first)
+                ? max
+                : curr.second * (heights.size() - curr.first);
+    }
+
+    return max;
+  }
+};
+
+int main() {
+  Solution s;
+
+  return 0;
+}
