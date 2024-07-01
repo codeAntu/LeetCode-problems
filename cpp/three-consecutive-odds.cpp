09b3fbec8534b6dbb08790de3868db2fee9ccb18
Ananta <codeantu@gmail.com>
2024-07-01 10:01:04 -0700

    kn

---
+++ cpp/three-consecutive-odds.cpp
@@ -0,0 +1,26 @@
+#include "vector"
+
+using namespace std;
+
+class Solution {
+public:
+  bool threeConsecutiveOdds(vector<int>& arr) {
+    int count = 0;
+    for (int i = 0; i < arr.size(); i++) {
+      if (arr[i] % 2 != 0) {
+        count++;
+        if (count == 3) {
+          return true;
+        }
+      } else
+        count = 0;
+    }
+    return false;
+  }
+};
+
+int main() {
+  Solution s;
+
+  return 0;
+}
