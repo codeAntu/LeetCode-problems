a816feafd32ff62da91770d763b33daeab174903
Ananta <codeantu@gmail.com>
2024-07-20 21:59:51 +0530

    kh

---
+++ java/find-valid-matrix-given-row-and-column-sums.java
@@ -0,0 +1,36 @@
+// class Solution {
+//     public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
+//         int m = rowSum.length;
+//         int n = colSum.length;
+
+//         int[][] res = new int[m][n];
+
+//         for (int i = 0; i < m; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 res[i][j] = Math.min(rowSum[i], colSum[j]);
+//                 rowSum[i] -= res[i][j];
+//                 colSum[j] -= res[i][j];
+//             }
+//         }
+
+//         return res;
+//     }
+// }
+
+
+class Solution {
+    public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
+        int m = rowSum.length;
+        int n = colSum.length;
+        
+        int[][] res = new int[m][n] ;
+        
+        for(int i = 0 ;i < m ; i++){
+            res[i][0] = rowSum[i] ;
+        }
+
+        for(int i = 0 ;i < n ; i++){
+            
+        }
+    }
+}
