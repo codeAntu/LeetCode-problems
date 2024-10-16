bf8c95b4b91da681feab3f4c8aa65a47a41baf69
Ananta <codeantu@gmail.com>
2024-10-16 13:46:32 +0530

    Rank 83,513

---
+++ cpp/longest-happy-string.cpp
@@ -0,0 +1,48 @@
+#include <queue>
+
+#include "iostream"
+#include "map"
+#include "math.h"
+#include "vector"
+
+using namespace std;
+
+class Solution {
+public:
+  string longestDiverseString(int a, int b, int c) {
+    priority_queue<pair<int, char>> pq;
+    string res = "";
+
+    if (a > 0) pq.push({a, 'a'});
+    if (b > 0) pq.push({b, 'b'});
+    if (c > 0) pq.push({c, 'c'});
+
+    while (!pq.empty()) {
+      auto [count1, char1] = pq.top();
+      pq.pop();
+
+      if (res.size() >= 2 && res[res.size() - 1] == char1 && res[res.size() - 2] == char1) {
+        if (pq.empty()) break;
+        auto [count2, char2] = pq.top();
+        pq.pop();
+        res += char2;
+        count2--;
+        if (count2 > 0) pq.push({count2, char2});
+        pq.push({count1, char1});
+      } else {
+        res += char1;
+        count1--;
+        if (count1 > 0) pq.push({count1, char1});
+      }
+    }
+
+    return res;
+  }
+};
+
+
+int main() {
+  Solution s;
+
+  return 0;
+}
