84668493329e69c1f07ce6e152a1d3c0f2090a1c
Ananta <codeantu@gmail.com>
2024-10-21 16:38:16 +0530

    Rank 82,287

---
+++ cpp/split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -0,0 +1,40 @@
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+
+#include "iostream"
+#include "map"
+#include "math.h"
+#include "vector"
+
+using namespace std;
+
+class Solution {
+public:
+  unordered_set<string> set;
+
+  int dfs(string & S, int index) {
+    if (index == S.size()) {
+      return 0;
+    }
+    int res = 0;
+    string temp = "";
+    for (int i = index; i < S.size(); i++) {
+      temp += S[i];
+      if (set.find(temp) == set.end()) {
+        set.insert(temp);
+        res = max(res, 1 + dfs(S, i + 1));
+        set.erase(temp);
+      }
+    }
+    return res;
+  }
+
+  int maxUniqueSplit(string s) { return dfs(s, 0); }
+};
+
+int main() {
+  Solution s;
+
+  return 0;
+}
