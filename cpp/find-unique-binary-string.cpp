aa7d927b99620e026db711c7afb79fd9b392759e
Ananta <codeantu@gmail.com>
2025-02-20 15:27:17 +0530

    Rank 57,504 , 702

---
+++ cpp/find-unique-binary-string.cpp
@@ -0,0 +1,29 @@
+#include <string>
+
+#include "vector"
+
+
+using namespace std;
+
+class Solution {
+public:
+  string findDifferentBinaryString(vector<string>& nums) {
+    string res = "";
+
+    for (int i = 0; i < nums.size(); i++) {
+      if (nums[i][i] == '0') {
+        res += '1';
+      } else {
+        res += '0';
+      }
+    }
+
+    return res;
+  }
+};
+
+int main() {
+  Solution s;
+
+  return 0;
+}
